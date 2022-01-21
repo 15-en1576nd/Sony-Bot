f87649859351ba6d87f5ea5863de037bfbe65620
Daneric <danericaalbers@hotmail.com>
2022-01-21 11:30:41 +0100

    feat: more buttons

--- SlashCommands/info/remote.js
+++ SlashCommands/info/remote.js
@@ -6,7 +6,7 @@ const { MessageActionRow, MessageButton } = require('discord.js');
 module.exports = {
     ...new SlashCommandBuilder()
     .setName("menu")
-    .setDescription("Control the TV"),
+    .setDescription("Control the TV."),
     /**
      *
      * @param {Client} client
@@ -20,8 +20,8 @@ module.exports = {
         const row = new MessageActionRow()
         .addComponents(
             new MessageButton()
-                .setCustomId('left')
-                .setEmoji('864550244276764682')
+                .setCustomId('on')
+                .setEmoji('496493394831343627')
                 .setStyle('PRIMARY'),
         )
         .addComponents(
@@ -29,7 +29,19 @@ module.exports = {
             .setCustomId('up')
             .setEmoji('864550244229316658')
             .setStyle('PRIMARY'),
-            );
+            )
+        .addComponents(
+            new MessageButton()
+                .setCustomId('off')
+                .setEmoji('827688850162974720')
+                .setStyle('PRIMARY'),
+        )
+        .addComponents(
+            new MessageButton()
+                .setCustomId('mute')
+                .setEmoji('925786264689606706')
+                .setStyle('PRIMARY'),
+        );
         const row2 = new MessageActionRow()
         .addComponents(
             new MessageButton()
@@ -37,24 +49,82 @@ module.exports = {
                 .setEmoji('864550244276764682')
                 .setStyle('PRIMARY'),
         )
+        .addComponents(
+            new MessageButton()
+                .setCustomId('enter')
+                .setEmoji('839998964752973866')
+                .setStyle('PRIMARY'),
+        )
+        .addComponents(
+            new MessageButton()
+                .setCustomId('right')
+                .setEmoji('864550244103880735')
+                .setStyle('PRIMARY'),
+        )
+        const row3 = new MessageActionRow()
+        .addComponents(
+            new MessageButton()
+                .setCustomId('none')
+                .setEmoji('934016556738961438')
+                .setStyle('PRIMARY'),
+        )
+        .addComponents(
+            new MessageButton()
+                .setCustomId('down')
+                .setEmoji('864550244192747561')
+                .setStyle('PRIMARY'),
+        )
+        .addComponents(
+            new MessageButton()
+                .setCustomId('none2')
+                .setEmoji('934016556738961438')
+                .setStyle('PRIMARY'),
+        )
             
             const embed = new MessageEmbed()
-        .setTitle('Menu')
-        .setDescription('Select an option');
+        .setTitle('Tv Remote')
+        .setDescription('Select an option ' + interaction.user.id);
 
-        await interaction.followUp({ embeds: [embed], components: [row] });
+        await interaction.followUp({ embeds: [embed], components: [row, row2, row3] });
 
-        const filter = i => i.user.id === interaction.member.id;
+        const filter = i => interaction.member.id === i.user.id;
 
-        const collector = interaction.channel.createMessageComponentCollector({ filter, time: 60000 });
+        const collector = interaction.channel.createMessageComponentCollector({ filter, time: 600000 });
 
         collector.on('collect', async i => {
             if (i.customId === 'left') {
-                await i.update({ content: 'A button was clicked!', components: [] });
+                tv.send("Left").catch(console.error)
+                embed.setDescription('Last Action Used: `Left`');
+                await i.update({ embeds: [embed] });
             }
             if (i.customId === 'up') {
                 tv.send("Up").catch(console.error)
                 embed.setDescription('Last Action Used: `Up`');
+                await i.update({ embeds: [embed] });
+            }
+            if (i.customId === 'down') {
+                tv.send("Down").catch(console.error)
+                embed.setDescription('Last Action Used: `Down`');
+                await i.update({ embeds: [embed] });
+            }
+            if (i.customId === 'right') {
+                tv.send("Right").catch(console.error)
+                embed.setDescription('Last Action Used: `Right`');
+                await i.update({ embeds: [embed] });
+            }
+            if (i.customId === 'on') {
+                tv.send("WakeUp").catch(console.error)
+                embed.setDescription('Last Action Used: `On`');
+                await i.update({ embeds: [embed] });
+            }
+            if (i.customId === 'off') {
+                tv.send("PowerOff").catch(console.error)
+                embed.setDescription('Last Action Used: `Off`');
+                await i.update({ embeds: [embed] });
+            }
+            if (i.customId === 'enter') {
+                tv.send("Enter").catch(console.error)
+                embed.setDescription('Last Action Used: `Enter`');
                 await i.update({ embeds: [embed] });
             }
         });
